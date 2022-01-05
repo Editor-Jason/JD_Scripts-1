d3633bccef148a399d8c5f88c8e0dbeba4218cdf
kirin <qq512554339@gmail.com>
2022-01-05 08:37:14 +0800

    读取收货人信息，windows用版本
    参考试用信息通知脚本简单修改后可以读取青龙内所有cookie收货人信息

---
+++ jd_addressinfo.py
@@ -0,0 +1,22 @@
+import requests,os
+
+cookie=''#pt_key=xxx;pt_pin=xxx;
+
+url='https://wq.jd.com/deal/recvaddr/getrecvaddrlistV3?adid=&locationid=undefined&callback=cbLoadAddressListA&reg=1&encryptversion=1&r=0.7347212362931181&sceneval=2'
+headers={
+    "accept":"*/*",
+    "accept-encoding":"gzip, deflate, br",
+    "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ca;q=0.5",
+    "cookie":cookie,
+    "referer":"https://wqs.jd.com/",
+    "sec-ch-ua":'" Not A;Brand";v="99", "Chromium";v="96", "Microsoft Edge";v="96"',
+    "sec-ch-ua-mobile":"?0",
+    "sec-ch-ua-platform":'"Windows"',
+    "sec-fetch-dest":"script",
+    "sec-fetch-mode":"no-cors",
+    "sec-fetch-site":"same-site",
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.34"
+    }
+response=requests.get(url=url,headers=headers)
+print(response.text)
+os.system('pause')
